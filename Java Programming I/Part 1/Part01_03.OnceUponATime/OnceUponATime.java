3a1e36bc2c038a6b85ffd866910ca8d65d34241d
Hrithik06 <el99el03@gmail.com>
2023-02-28 19:33:17 +0530

    Updated Directory

--- Java Programming I/Part 1/Part01_03.OnceUponATime/OnceUponATime.java
+++ Java Programming I/Part 1/Part01_03.OnceUponATime/OnceUponATime.java
@@ -1,6 +1,5 @@
-
 public class OnceUponATime {
-
+ 
     public static void main(String[] args) {
         // Write your program here
         System.out.println("Once upon a time");
--- Java Programming I/Part 1/Part01_03.OnceUponATime/src/java/OnceUponATime.java
+++
@@ -1,7 +0,0 @@
-
-public class OnceUponATime {
-
-    public static void main(String[] args) {
-        // Write your program here
-    }
-}

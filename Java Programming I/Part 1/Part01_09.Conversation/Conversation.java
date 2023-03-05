138c92188cee230f4791b2d23a8f2c8c29f634e6
Hrithik06 <el99el03@gmail.com>
2023-03-05 13:18:08 +0530

    Added Part01_09.Conversation

---
+++ Java Programming I/Part 1/Part01_09.Conversation/Conversation.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Conversation {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        
+        System.out.println("Greetings! How are you doing?");
+        
+        String string1 = scanner.nextLine();
+        
+        System.out.println("Oh, how interesting. Tell me more!");
+        
+        String string2 = scanner.nextLine();
+        
+        System.out.println("Thanks for sharing!");
+
+    }
+}

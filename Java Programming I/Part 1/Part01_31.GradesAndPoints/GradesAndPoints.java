21b5f5cb7fff299f7b87b0c4606de86533f9c05a
Hrithik06 <el99el03@gmail.com>
2023-07-31 19:42:47 +0530

    Added GradesAndPoints

---
+++ Java Programming I/Part 1/Part01_31.GradesAndPoints/GradesAndPoints.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class GradesAndPoints {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give points [0-100]:");
+        
+        int points = Integer.valueOf(scan.nextLine());
+        
+        if(points>100){
+            System.out.println("Grade: incredible!");
+        }else if(points<=100 && points>=90){
+            System.out.println("Grade: 5");
+        }else if(points<90 && points>=80){
+            System.out.println("Grade: 4");
+        }else if(points<80 && points>=70){
+            System.out.println("Grade: 3");
+        }else if(points<70 && points>=60){
+            System.out.println("Grade: 2");
+        }else if(points<60 && points>=50){
+            System.out.println("Grade: 1");
+        }else if(points<50 && points>=0){
+            System.out.println("Grade: failed");
+        }else if(points<0){
+            System.out.println("Grade: impossible!");
+        }
+
+    }
+}
